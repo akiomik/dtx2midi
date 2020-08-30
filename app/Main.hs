577ebc9e9b3789ddedc01f87018048dca73bf04f
Akiomi Kamakura <akiomik@gmail.com>
2020-08-31 06:46:04 +0900

    Add file encoding check

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,16 @@
 module Main where
 
+import Codec.Text.Detect (detectEncodingName)
 import DTX2MIDI (dtxToMIDI, fromDTXFile, toMIDIFile)
+import qualified Data.ByteString.Lazy as B
 import Data.Semigroup ((<>))
 import Data.Version (showVersion)
 import Options.Applicative
 import qualified Paths_dtx2midi
 
 data Opts = Opts
-  { input :: String,
-    output :: String
+  { input :: FilePath,
+    output :: FilePath
   }
 
 opts :: Parser Opts
@@ -29,12 +31,24 @@ opts =
 
 run :: Opts -> IO ()
 run (Opts input output) = do
-  putStrLn $ "Input file: " ++ input
-  putStrLn $ "Output file: " ++ output
+  putStrLn $ "[INFO] Input file: " ++ input
+  putStrLn $ "[INFO] Output file: " ++ output
+  checkInputEncoding input
   dtx <- fromDTXFile input
   midi <- dtxToMIDI dtx
   toMIDIFile output midi
-  putStrLn "Complete!"
+  putStrLn "[INFO] Complete!"
+
+checkInputEncoding :: FilePath -> IO ()
+checkInputEncoding fp = do
+  file <- B.readFile fp
+  case detectEncodingName file of
+    Just enc | enc `elem` ["ASCII", "UTF-8", "UTF-16BE", "UTF-16LE", "UTF-32BE", "UTF-32LE"] -> do
+      return ()
+    Just enc -> do
+      putStrLn $ "[WARN] The conversion may fail due to an unsupported encoding '" ++ enc ++ "' being detected in " ++ fp ++ "."
+    Nothing -> do
+      putStrLn $ "[WARN] The conversion may fail due to an unknown encoding being detected in " ++ fp ++ "."
 
 version :: String
 version = showVersion Paths_dtx2midi.version
